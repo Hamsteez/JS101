448ef70a1f6e5ecb566e9af177a7bc0cd737825e
Hamsteez <hamza.y.c@gmail.com>
2021-01-17 22:23:53 -0500

    updated mortgage calc based on code review

--- lesson_2/mortgage.js
+++ lesson_2/mortgage.js
@@ -1,38 +1,59 @@
-let uInput = require('readline-sync');
+let readline = require('readline-sync');
 
 console.log('Welcome to the monthly mortgage payment calculator!');
 
-function numTest(num) {
+function numTestLoanApr(num) {
+  return num.toString().trim() === '' ||
+         num <= 0   ||
+         Number.isNaN(Number(num));
+}
+
+function numTestDur(num) {
   return num.toString().trim() === '' ||
          num < 0   ||
          Number.isNaN(Number(num));
 }
 
+function numTestIntDur(num) {
+  return ((num % 1) !== 0);
+}
+
 let keepGoing = true;
 
 while (keepGoing) {
-  let loanAmount = (uInput.question('What is the total loan amount (i.e 5000 is $5000)? -->'));
-  while (numTest(loanAmount)) {
+  let loanAmount = (readline.question('What is the total loan amount (i.e 5000 is $5000)? -->'));
+  while (numTestLoanApr(loanAmount)) {
     console.log('Please enter an appropriate value -->');
-    loanAmount = Number(uInput.question());
+    loanAmount = Number(readline.question());
   }
 
-  let apr = Number(uInput.question('What is the Annual Percentage Rate (i.e 5 is 5%)? -->'));
-  while (numTest(apr)) {
+  let apr = Number(readline.question('What is the Annual Percentage Rate (i.e 5 is 5%)? -->'));
+  while (numTestLoanApr(apr)) {
     console.log('Please enter an appropriate value -->');
-    apr = Number(uInput.question());
+    apr = Number(readline.question());
   }
 
-  let loanDurYears = Number(uInput.question('What is total number of years the loan will be (i.e 5 is years) -->?'));
-  while (numTest(loanDurYears)) {
+  let loanDurYears = Number(readline.question('What is total number of years the loan will be (i.e 5 is 5 years) -->?'));
+  while (numTestDur(loanDurYears) || numTestIntDur(loanDurYears)) {
     console.log('Please enter an appropriate value -->');
-    loanDurYears = Number(uInput.question());
+    loanDurYears = Number(readline.question());
   }
 
-  let loanDurMonths = Number(uInput.question('Any extra months in addition the the number of years? (i.e 5 is 5 months)? -->'));
-  while (numTest(loanDurMonths)) {
+  let loanDurMonths = Number(readline.question('Any extra months in addition to the number of years? (i.e 5 is 5 months)? -->'));
+  if (numTestIntDur(loanDurMonths)) {
+    console.log('ok');
+  }
+  while (numTestDur(loanDurMonths) || numTestIntDur(loanDurMonths)) {
     console.log('Please enter an appropriate value -->');
-    loanDurMonths = Number(uInput.question());
+    loanDurMonths = Number(readline.question());
+  }
+
+  while (loanDurYears === 0 && loanDurMonths === 0) {
+    console.log('You entered a value of 0 for both years and months, please enter a value for either years or months, Thank you -->');
+    console.log('Please enter a positive value for years -->');
+    loanDurYears = Number(readline.question());
+    console.log('Please enter a positive value for months -->');
+    loanDurMonths = Number(readline.question());
   }
 
   let monthlyIntRate = (apr / 100) / 12;
@@ -41,17 +62,15 @@ while (keepGoing) {
   let monthlyPayment = loanAmount *
     (monthlyIntRate / (1 - Math.pow((1 + monthlyIntRate), (-totLoanMonths))));
 
-  // eslint-disable-next-line max-len
-  //console.log(loanAmount, apr, loanDurYears, loanDurMonths, monthlyIntRate, totLoanMonths);
-
   console.log(`Your monthly payment is: $${monthlyPayment.toFixed(2)}`);
 
-  let go = uInput.question('Would you like another calculation? (Enter yes/no) -->').toLowerCase();
-  while (go !== 'yes' && go !== 'no') {
-    go = uInput.question('Please enter yes/no) -->').toLowerCase();
+  let go = readline.question('Would you like another calculation? (Enter yes/no) -->').toLowerCase();
+  const VALID_CHOICES = ['yes', 'no', 'y', 'n'];
+  while (!VALID_CHOICES.includes(go)) {
+    go = readline.question('Please enter yes/no -->').toLowerCase();
   }
 
-  if (go === 'no') {
+  if (go === 'no' || go === 'n') {
     keepGoing = false;
     console.log('Have a good day!');
   }
