08180c62e403ed70354104e739a3499f0318ee12
Hamsteez <hamza.y.c@gmail.com>
2021-01-18 21:47:56 -0500

    RPS implemented code review notes

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,24 +1,56 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 const SHORTHAND_CHOICES = ['r', 'p', 'sc', 'l', 'sp'];
+const CONTINUE_CHOICES = ['y', 'n', 'yes', 'no'];
+const WINNING_MOVES = {
+  r: ['sc', 'l'],
+  p: ['r', 'sp'],
+  sc: ['p', 'l'],
+  l: ['p', 'sp'],
+  sp: ['r', 'sc'],
+};
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function displayWinner(choice, computerChoice) {
+function choiceConverter(choice) {
+  switch (choice) {
+    case 'rock':
+      choice = 'r';
+      break;
+    case 'paper':
+      choice = 'p';
+      break;
+    case 'scissors':
+      choice = 'sc';
+      break;
+    case 'lizard':
+      choice = 'l';
+      break;
+    case 'spock':
+      choice = 'sp';
+      break;
+  }
+  return choice;
+}
+
+function displayChoice(choice, computerChoice) {
   let myChoice = SHORTHAND_CHOICES.indexOf(choice);
   let compChoice = SHORTHAND_CHOICES.indexOf(computerChoice);
-  prompt(`You chose ${VALID_CHOICES[myChoice]}, computer chose ${VALID_CHOICES[compChoice]}`);
+  prompt(`You chose ${VALID_CHOICES[myChoice]}, computer chose ${VALID_CHOICES[compChoice]}.`);
+}
 
-  if (youWin(choice, computerChoice)) {
-    prompt('You won that round');
-    scoreKeeper('human');
-  } else if (youLose(choice, computerChoice)) {
-    prompt('Computer won that round');
-    scoreKeeper('bot');
+function roundWon(choice, computerChoice) {
+  if (whoWon(choice, computerChoice)) {
+    prompt('You won that round.');
+    return 'human';
+  } else if (whoWon(computerChoice, choice)) {
+    prompt('Computer won that round.');
+    return 'bot';
   } else {
     prompt("That round was a tie! Score is unchanged.");
+    return 'tie';
   }
 }
 
@@ -29,73 +61,86 @@ let botScore = 0;
 function scoreKeeper(roundWinner) {
   if (roundWinner === 'human') {
     humanScore++;
-  } else {
+  } else if (roundWinner === 'bot') {
     botScore++;
   }
+}
 
+function displayScore() {
   if (humanScore === 5) {
-    prompt(`You win best of 5: Your score ${humanScore}, Computer score: ${botScore}`);
-    matchOngoing = false;
+    prompt(`You win best of 5: Your score ${humanScore}, Computer score: ${botScore}.`);
+  } else if (botScore === 5) {
+    prompt(`You lose best of 5: Your score ${humanScore}, Computer score: ${botScore}.`);
+  } else {
+    prompt(`You got this! The current score is: you: ${humanScore} computer: ${botScore}.`);
+  }
+}
+
+function updateScore() {
+  if (humanScore === 5) {
     humanScore = 0;
     botScore = 0;
   } else if (botScore === 5) {
-    prompt(`You lose best of 5: Your score ${humanScore}, Computer score: ${botScore}`);
-    matchOngoing = false;
     humanScore = 0;
     botScore = 0;
-  } else {
-    prompt(`You got this! The current score is: you: ${humanScore} computer: ${botScore}`);
   }
-
 }
 
-function youWin(choice, computerChoice) {
-  return (choice === 'sc' && computerChoice === 'p') ||
-      (choice === 'p' && computerChoice === 'r') ||
-      (choice === 'r' && computerChoice === 'l') ||
-      (choice === 'l' && computerChoice === 'sp') ||
-      (choice === 'sp' && computerChoice === 'sc') ||
-      (choice === 'sc' && computerChoice === 'l') ||
-      (choice === 'l' && computerChoice === 'p') ||
-      (choice === 'p' && computerChoice === 'sp') ||
-      (choice === 'sp' && computerChoice === 'r') ||
-      (choice === 'r' && computerChoice === 'sc');
+function isGameOver() {
+  if (humanScore === 5) {
+    matchOngoing = false;
+  } else if (botScore === 5) {
+    matchOngoing = false;
+  }
 }
 
-function youLose(choice, computerChoice) {
-  return (computerChoice === 'sc' && choice === 'p') ||
-      (computerChoice === 'p' && choice === 'r') ||
-      (computerChoice === 'r' && choice === 'l') ||
-      (computerChoice === 'l' && choice === 'sp') ||
-      (computerChoice === 'sp' && choice === 'sc') ||
-      (computerChoice === 'sc' && choice === 'l') ||
-      (computerChoice === 'l' && choice === 'p') ||
-      (computerChoice === 'p' && choice === 'sp') ||
-      (computerChoice === 'sp' && choice === 'r') ||
-      (computerChoice === 'r' && choice === 'sc');
+function matchIsOngoing() {
+  return matchOngoing;
+}
+
+function whoWon(choice, computerChoice) {
+  return WINNING_MOVES[choice].includes(computerChoice);
+}
+
+function userChooses() {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}; key: r p sc sp l`);
+  let userChoice = readline.question().toLowerCase();
+  while (!SHORTHAND_CHOICES.includes(userChoice) &&
+     !VALID_CHOICES.includes(userChoice)) {
+    prompt("That's not a valid choice");
+    userChoice = readline.question().toLowerCase();
+  }
+  return userChoice;
+}
+
+function computerChooses() {
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let compChoice = SHORTHAND_CHOICES[randomIndex];
+  return compChoice;
 }
 
 while (true) {
-  while (matchOngoing) {
-    prompt(`Choose one: ${VALID_CHOICES.join(', ')}; key: r p sc sp l`);
-    let choice = readline.question();
-
-    while (!SHORTHAND_CHOICES.includes(choice)) {
-      prompt("That's not a valid choice");
-      choice = readline.question();
-    }
-
-    let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-    let computerChoice = SHORTHAND_CHOICES[randomIndex];
-
-    displayWinner(choice, computerChoice);
+  while (matchIsOngoing()) {
+    let choice = userChooses();
+    choice = choiceConverter(choice);
+    let computerChoice = computerChooses();
+    displayChoice(choice, computerChoice);
+    let pointTracker = roundWon(choice, computerChoice);
+    scoreKeeper(pointTracker);
+    displayScore();
+    isGameOver();
+    updateScore();
   }
   prompt('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
+  while (!CONTINUE_CHOICES.includes(answer)) {
     prompt('Please enter "y" or "n".');
     answer = readline.question().toLowerCase();
   }
 
-  if (answer[0] !== 'y') break;
+  if (answer === 'y' || answer === 'yes') {
+    matchOngoing = true;
+  } else {
+    break;
+  }
 }
